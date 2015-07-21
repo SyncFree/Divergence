0c38eabeac3ef4f6084608e5de20fa81d0b81735
jleitao <jc.leitao@fct.unl.pt>
2015-07-21 16:04:09 +0100

    new support for database observable interface

--- Peersim-DC/src/peersim/core/dcdatastore/observers/dbstate/DatabaseObserver.java
+++ Peersim-DC/src/peersim/core/dcdatastore/observers/dbstate/DatabaseObserver.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import peersim.config.Configuration;
 import peersim.core.Control;
+import peersim.core.dcdatastore.DCCommonState;
 import peersim.core.dcdatastore.GeoReplicatedDatastoreNetwork;
 import peersim.core.dcdatastore.ServerNode;
 import peersim.core.dcdatastore.util.DataObject;
@@ -30,10 +31,11 @@ public class DatabaseObserver implements Control {
 	}
 
 	public boolean execute() {
+		this.output.println("Database State report: " + DCCommonState.getTime());
 		for( int i = 1; i <= GeoReplicatedDatastoreNetwork.numberDCs(); i++ ) {
 			ServerNode servers[] = GeoReplicatedDatastoreNetwork.getServersForDC(i);
 			for (int j = 0; j < servers.length; j++) {
-				this.output.println("Server " + j + " (DC: " + j + ")");
+				this.output.println("Server " + j + " (DC: " + i + ")");
 				Map<String,DataObject<?,?>> dbCopy = ((DatabaseObservable)servers[j].getProtocol(protocolID)).getDatabaseState((ServerNode) servers[j]);
 				for(String k: dbCopy.keySet()) {
 					this.output.println("Key " + k + " -> " + dbCopy.get(k).toString());
